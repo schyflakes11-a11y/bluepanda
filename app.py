46676ed80baeb4f74ab3c3c240ae65c5caaad3d4
schyflakes11-a11y <schyflakes11@gmail.com>
2026-04-29 20:34:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from random import *
 app = Flask(__name__)
 @app.route("/")
 def home():
-    random_number = rannum(1, 100)
+    random_number = randint(1, 100)
     text = f"Your random number is: {random_number}"
     return text
 if __name__ == "__main__":

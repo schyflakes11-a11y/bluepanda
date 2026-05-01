07737363d287e8bdea79c44b4cb0e1124d61c833
schyflakes11-a11y <schyflakes11@gmail.com>
2026-05-01 11:00:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ app = Flask(__name__)
 def home():
     text = """
     Day 1: I made a new website. webflakes.qzz.io, it will contain this website and my projects (since i cannot get anymore domains.)
-    Day 2: I made a Random Number Generator, i made it with simple HTML and a bit of CSS. I learned about Jinja because of this.
+    Day 2: I made a Random Number Generator, i made it with simple HTML and a bit of CSS. I learned about Jinja and Requests because of this.
+    
     
     
     """
+    return text
 if __name__ == "__main__":
     app.run()
